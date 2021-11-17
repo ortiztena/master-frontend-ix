14f78b5650618af30fe98353baa254c1f9286872
ortiztena <ortiztena@gmail.com>
2021-11-17 18:00:32 +0100

    commit

---
+++ 05-testing/02-optional-cypress/config/test/jest.js
@@ -0,0 +1,8 @@
+module.exports = {
+  rootDir: '../../',
+  preset: 'ts-jest',
+  restoreMocks: true,
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/config/test/setup-after.ts'],
+  moduleDirectories: ['<rootDir>/src', 'node_modules'],
+};
