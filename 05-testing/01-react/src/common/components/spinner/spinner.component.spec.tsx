70a6fa0dff9155d900e966229c5b02943605b8a9
ortiztena <ortiztena@gmail.com>
2021-11-17 14:03:49 +0100

    commit

--- 05-testing/01-react/src/common/components/spinner/spinner.component.spec.tsx
+++ 05-testing/01-react/src/common/components/spinner/spinner.component.spec.tsx
@@ -5,33 +5,6 @@ import userEvent from '@testing-library/user-event';
 
 describe(' # spinner.component spec', () => {
   it('', () => {
-    render(<SpinnerComponent />);
-    const dialogElement = screen.getByRole('dialog');
     // Assert
-    expect(dialogElement).toBeInTheDocument();
-    expect(dialogElement).not.toBeNull();
-    expect(dialogElement).toBeTruthy();
-  });
-
-  it('should render/test click, a selected element: #button# ', () => {
-    // Arrange
-
-    // Act
-    render(<SpinnerComponent />);
-    const closeButton = screen.getByRole('button', { name: /testClose/i });
-    const acceptButton = screen.getByRole('button', { name: /testAccept/i });
-    userEvent.click(closeButton);
-    userEvent.click(acceptButton);
-    // Assert
-    expect(closeButton).toBeInTheDocument();
-    expect(acceptButton).toBeInTheDocument();
-    expect(acceptButton).not.toBeNull();
-    expect(closeButton).not.toBeNull();
-  });
-
-  it('', () => {
-    // Arrange
-    // Assert
-    // Act
   });
 });
