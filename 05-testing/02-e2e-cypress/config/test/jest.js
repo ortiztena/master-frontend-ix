53eda5712198a140b494164a665d54dc517b48c6
ortiztena <56440569+ortiztena@users.noreply.github.com>
2021-11-18 16:01:56 +0100

    Delete 05-testing/02-e2e-cypress directory

--- 05-testing/02-e2e-cypress/config/test/jest.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  rootDir: '../../',
-  preset: 'ts-jest',
-  restoreMocks: true,
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/config/test/setup-after.ts'],
-  moduleDirectories: ['<rootDir>/src', 'node_modules'],
-};
