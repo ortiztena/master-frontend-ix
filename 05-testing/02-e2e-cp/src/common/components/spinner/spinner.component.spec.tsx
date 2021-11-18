cb50ba7288930fc93b1dae82bdb08a3ecec1a499
ortiztena <ortiztena@gmail.com>
2021-11-18 16:06:37 +0100

    commit

---
+++ 05-testing/02-e2e-cp/src/common/components/spinner/spinner.component.spec.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import { SpinnerComponent } from './spinner.component';
+import * as mocked from 'react-promise-tracker/lib/trackerHook';
+
+describe('Spinner.component spec', () => {
+  it('', () => {
+    //   // Arrange
+    //   const promiseTrackerStub = jest
+    //     .spyOn(mocked, 'usePromiseTracker')
+    //     .mockImplementation(() => ({ promiseInProgress: true }));
+    //   // Act
+    //   render(<SpinnerComponent />);
+    //   const presentation = screen.findByRole('presentation');
+    //   // Assert
+    //   // expect(promiseTrackerStub).toHaveBeenCalled();
+    //   expect(presentation).toBeInTheDocument();
+  });
+});
