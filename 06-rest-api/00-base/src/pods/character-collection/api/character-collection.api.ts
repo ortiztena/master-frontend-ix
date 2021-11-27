c66f052f9be50dde6675f1d1a7b36192640339a4
ortiztena <ortiztena@gmail.com>
2021-11-27 18:45:30 +0100

    commit

---
+++ 06-rest-api/00-base/src/pods/character-collection/api/character-collection.api.ts
@@ -0,0 +1,13 @@
+// import { HotelEntityApi } from './character-collection.api-model';
+// import { mockHotelCollection } from './character-collection.mock-data';
+
+// let hotelCollection = [...mockHotelCollection];
+
+// export const getHotelCollection = async (): Promise<HotelEntityApi[]> => {
+//   return hotelCollection;
+// };
+
+// export const deleteHotel = async (id: string): Promise<boolean> => {
+//   hotelCollection = hotelCollection.filter((h) => h.id !== id);
+//   return true;
+// };
