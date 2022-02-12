1bc4b334d11fe0b759d15cb9a77a8834c61eda49
ortiztena <ortiztena@gmail.com>
2022-02-12 18:47:55 +0100

    demo vue

---
+++ 04-frameworks/03-vue/02-vue-demo/ecommerce-app/src/services/products.ts
@@ -0,0 +1,10 @@
+import { Product } from '@/types'
+
+export const productService = {
+  async get(): Promise<Product[]> {
+    const books = await import(
+      './books.mock.json'
+    ).then(m => m.default)
+    return books
+  },
+}
